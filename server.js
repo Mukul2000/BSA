cfef4a7f1c88097e1d20d9f21d323fa83bceab86
Mukul <mukulkumar627@gmail.com>
2022-04-19 16:46:53 +0530

    Add GET /countries endpoint

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
 
-const route = require('./routes/route');
-
-
 const app = express();
 
-app.use('/api', route);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
 
@@ -16,4 +12,14 @@ app.listen(process.env.PORT || 8000, () => {
     console.log("Server started on port 8000");
 });
 
+app.get('/countries', async (req,res) => {
+    db.all("SELECT id, MIN(YEAR), MAX(YEAR), country_or_area from emissions group by country_or_area", (err,rows) => {
+        if(err) {
+            console.log(err);
+            res.status(500).json({"error": ["Something went wrong", err]});
+        }
+        else 
+            res.status(200).json(rows);
+    });
+});
 
